d75d26b93a143e7d166b0e7c628486b3e6a59a2d
Nikita Puh <isedeku@mail.ru>
2021-05-08 19:36:36 +0300

    Исправление кнопки "Категории"

--- handlers/users/categories.py
+++ handlers/users/categories.py
@@ -1,39 +1,9 @@
 from aiogram import types
 
+from keyboards.inline.category import menu_4
 from loader import dp
 
 
 @dp.message_handler(text='🧾 Категории')
 async def categories(message: types.Message):
-    await message.answer(
-        'Туристичні навігатори:\nhttps://aparat.ua/turisticheskie-navigatori/'
-        '\nЕхолоти та картплоттери:\nhttps://aparat.ua/eholoty-i-kartplottery/'
-        '\nАксесуари для спорту та фітнесу:'
-        '\nhttps://aparat.ua/sport-i-fitnes/'
-        '\nGps навігатори:\nhttps://aparat.ua/gps-navigatory/'
-        '\nВелоспорт:\nhttps://aparat.ua/velosport/'
-        '\nКамери і Відеореєстратори:\nhttps://aparat.ua/kamery-i-videoregistratory/'
-        '\nКарти для GPS Навігаторів:\nhttps://aparat.ua/karti-dlya-navigatorov/'
-        '\nАксесуари для Garmin:\nhttps://aparat.ua/aksessuary-dlya-garmin/'
-        '\nКаталог товарів:\nhttps://aparat.ua/drugie-kategorii/'
-        '\nКомпанія Аппарат:\nКупити Туристичні Навігатори в Києві. Порівняти Ціни по Україні | aparat.ua'
-        '\nКупуйте Туристичні GPS Навігатори в інтернет-магазин aparat.ua ?100% Оригінал :heavy_check_mark:'
-        'Проверенное Якість ?Доступние ціни :airplane:Бистрая доставка в усі регіони Укра...'
-        '\nhttps://aparat.ua/servisnye-tsentry">Сервісні центри'
-        '\nhttps://aparat.ua/ustanovka-kart">Установка карт'
-        '\nhttps://aparat.ua/remont-navigatorov">Ремонт навігаторів'
-        '\nhttps://aparat.ua/remont-chasov-i-brasletov-garmin">Ремонт годинників та браслетів Garmin'
-        '\nhttps://aparat.ua/ustanovka-oborudovaniya">Встановлення обладнання'
-        '\nhttps://aparat.ua/trade-in-prokachaj-svoj-staryj-navigator-ili-chasy-na-novyj">Послуга Trade-in'
-        '\nКомпанія Аппарат:'
-        '\nСервісний центри Garmin (Гармін). Офіційний сервіс Гармін (Garmin) | aparat.ua'
-        '\n?Сервісний центри Garmin? Купуйте >Звоніте :phone: 0956956905 :phone: 0964304543 :heavy_check_mark:'
-        'Лучшая Ціна :heavy_check_mark:Офіціальная Гарантія :airplane:Прайс на Послуги'
-        '\nВідгуки Facebook:'
-        '\nhttps://www.facebook.com/apparatgps/reviews/'
-        '\nВідгуки Google:'
-        '\nhttps://www.google.com/search?q=отзывы+aparat.ua&sxsrf=ALeKk00oYaqBfhw3L8C50XF0ml2ZiUy_'
-        'DA:1617353061189&ei=ZdlmYMj2Cs39rgTNl7yADQ&oq=отзывы+aparat.ua&gs_lcp=Cgdnd3Mtd2l6EAMyBAgjECcyBgg'
-        'AEAgQHjoHCCMQsAMQJ1D7iwFY948BYNuTAWgBcAB4AIABqgGIAcMCkgEDMi4xmAEAoAEBqgEHZ3dzLXdpesgBAcABAQ&sclient='
-        'gws-wiz&ved=0ahUKEwiIvcDVld_vAhXNvosKHc0LD9AQ4dUDCA0&uact=5', disable_web_page_preview=True
-    )
+    await message.answer('Вот все категории', reply_markup=menu_4)
---
+++ keyboards/inline/category.py
@@ -0,0 +1,43 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+menu_4 = InlineKeyboardMarkup(row_width=2)
+
+BUTTON_1 = 'https://aparat.ua/turisticheskie-navigatori/'
+button_1 = InlineKeyboardButton(text='Туристичні навігатори', url=BUTTON_1)
+menu_4.insert(button_1)
+
+BUTTON_2 = 'https://aparat.ua/eholoty-i-kartplottery/'
+button_2 = InlineKeyboardButton(text='Ехолоти та картплоттери', url=BUTTON_2)
+menu_4.insert(button_2)
+
+BUTTON_3 = 'https://aparat.ua/sport-i-fitnes/'
+button_3 = InlineKeyboardButton(text='Аксесуари для спорту та фітнесу', url=BUTTON_3)
+menu_4.insert(button_3)
+
+BUTTON_4 = 'https://aparat.ua/gps-navigatory/'
+button_4 = InlineKeyboardButton(text='Gps навігатори', url=BUTTON_4)
+menu_4.insert(button_4)
+
+BUTTON_5 = 'https://aparat.ua/velosport/'
+button_5 = InlineKeyboardButton(text='Велоспорт', url=BUTTON_5)
+menu_4.insert(button_5)
+
+BUTTON_6 = 'https://aparat.ua/kamery-i-videoregistratory/'
+button_6 = InlineKeyboardButton(text='Камери і Відеореєстратори', url=BUTTON_6)
+menu_4.insert(button_6)
+
+BUTTON_7 = 'https://aparat.ua/karti-dlya-navigatorov/'
+button_7 = InlineKeyboardButton(text='Карти для GPS Навігаторів', url=BUTTON_7)
+menu_4.insert(button_7)
+
+BUTTON_8 = 'https://aparat.ua/aksessuary-dlya-garmin/'
+button_8 = InlineKeyboardButton(text='Аксесуари для Garmin', url=BUTTON_8)
+menu_4.insert(button_8)
+
+BUTTON_9 = 'https://aparat.ua/drugie-kategorii/'
+button_9 = InlineKeyboardButton(text='Каталог товарів', url=BUTTON_9)
+menu_4.insert(button_9)
+
+
+
+
